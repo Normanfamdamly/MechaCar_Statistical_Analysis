bc0a5f728394d6dd5f35d2d8db20918389dbb8c2
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-10-09 21:31:43 -0400

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,11 @@ head(MechaCar_mpg)
 # linear regression model
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
+
+
+# Deliverable 2 Create Visualizations for the Trip Analysis
+# import csv and read as a table
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#stat summary with summarize()
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
